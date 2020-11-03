a6d918b150ab4cb4dd3217545ea89497d77b64e3
Co0perator <69565184+Co0perator@users.noreply.github.com>
2020-11-03 09:18:38 -0600

    Added what I've got so far
    I added what I have and what I have is very little

---
+++ PAIN_Interpreter/PAIN_Interpreter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+	if (argc > 2)
+	{
+		std::out << "More arguments given then needed. All we need is the file name, and the interpreter takes care of the rest ;)" << std::endl;
+		return 0;
+	}
+	
+}
